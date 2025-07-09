a9d6b8da42856149595186fa4acf1656a39b219e
ryanabdallahconrad <ryanconrad1@zohomail.com>
2025-07-09 10:50:30 -0500

    set up heroku frontend automated build

--- src/spotify-frontend/src/App.jsx
+++ src/spotify-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import Upload from './components/Upload'
-import MainSpotifyStatsUI from './components/MainSpotifyStatsUI'
+import Upload from './components/Upload.jsx'
+import MainSpotifyStatsUI from './components/MainSpotifyStatsUI.jsx'
 
 export default function App() {
     const [uploadComplete, setUploadComplete] = useState(false)
